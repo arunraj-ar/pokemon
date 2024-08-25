50a261723a59f09a2a226a7941da6c65070321ec
arunraj-ar <mail.arunrajar@gmail.com>
2024-08-26 02:28:40 +0530

    adde filters dummy component

--- src/ListingPage.jsx
+++ src/ListingPage.jsx
@@ -4,7 +4,7 @@ export const ListingPage = (props) => {
   return (
     <div className="flex flex-col items-center justify-center mt-80">
       <h1 className="text-5xl sticky top-0 w-dvw lg:w-1/3 bg-[#242424] p-4">Pokemons</h1>
-      {/* <div className="w-1/3 m-10 ml-16">filters</div> */}
+      <div className="w-1/3 m-10 ml-16">filters</div>
       <Pokemons />
     </div>
   );
