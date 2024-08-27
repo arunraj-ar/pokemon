bd5eab38094913edc9a922be63476e8ce6bb257c
arunraj-ar <mail.arunrajar@gmail.com>
2024-08-27 23:57:57 +0530

    react window added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Pokemons } from "./Pokemons";
 import { NotFound } from "./NotFound";
 import { Credits } from "./Credits";
 import { ListingPage } from "./ListingPage";
@@ -12,8 +11,8 @@ function App() {
           <Route path="/" element={<Navigate to="/pokemons" />} exact />
           <Route path="/home" element={<Navigate to="/" />}  exact/>
           <Route path="/pokemons" element={<ListingPage />} />
-          <Route path="*" element={<NotFound />} />
           <Route path="/credits" element={<Credits />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
