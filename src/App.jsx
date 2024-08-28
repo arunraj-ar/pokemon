781e2a1b7c2b9b13d01f67aa47d89c444d062931
arunraj-ar <mail.arunrajar@gmail.com>
2024-08-29 02:22:03 +0530

    protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
 import { lazy, Suspense } from "react";
 import PageLoader from "./PageLoader";
 
 const ListingPage = lazy(() => import("./ListingPage"));
 const NotFound = lazy(() => import("./NotFound"));
 const Credits = lazy(() => import("./Credits"));
+
+function AuthRoute() {
+  const user = localStorage.getItem("user");
+  return user ? <Outlet /> : <Navigate to="/" />;
+}
 
 function App() {
   return (
@@ -15,7 +26,9 @@ function App() {
             <Route path="/" element={<Navigate to="/pokemons" />} exact />
             <Route path="/home" element={<Navigate to="/" />} exact />
             <Route path="/pokemons" element={<ListingPage />} />
-            <Route path="/credits" element={<Credits />} />
+            <Route element={<AuthRoute />}>
+              <Route path="/credits" element={<Credits />} />
+            </Route>
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Suspense>
